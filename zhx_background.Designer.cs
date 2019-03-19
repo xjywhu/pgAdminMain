f4a95e97e4f79c885d4b2a575d92294b61232bc0
HongxuanZhang <1173071213@qq.com>
2019-03-19 19:22:36 +0800

    实现部分功能

---
+++ zhx_background.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace pgAdminMain
+{
+    partial class zhx_background
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.zhx_submit = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(15, 584);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(490, 133);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // zhx_submit
+            // 
+            this.zhx_submit.Location = new System.Drawing.Point(526, 584);
+            this.zhx_submit.Name = "zhx_submit";
+            this.zhx_submit.Size = new System.Drawing.Size(176, 36);
+            this.zhx_submit.TabIndex = 5;
+            this.zhx_submit.Text = "提交指令";
+            this.zhx_submit.UseVisualStyleBackColor = true;
+            this.zhx_submit.Click += new System.EventHandler(this.zhx_submit_Click_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 30;
+            this.dataGridView1.Size = new System.Drawing.Size(687, 545);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // zhx_background
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.zhx_submit);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "zhx_background";
+            this.Size = new System.Drawing.Size(716, 723);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button zhx_submit;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ zhx_background.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace pgAdminMain
+{
+    public partial class zhx_background : UserControl
+    {
+        public zhx_background()
+        {
+            InitializeComponent();
+        }
+
+        private static String GetTableName(String str)
+        {
+            int index = 0;
+            if ((index = str.IndexOf("from", StringComparison.OrdinalIgnoreCase)) < 0)
+            {
+                return null;
+            }
+            //find next space
+            int index_1 = index + 4;
+            while (str[index_1] == ' ') { index_1 += 1; }
+            int index_2 = str.IndexOf(' ', index_1 + 1);
+            if (index_2 == -1)
+            {
+                return str.Substring(index_1);
+            }
+            else
+                return str.Substring(index_1, index_2-index_1);
+        }
+
+        private void zhx_submit_Click_1(object sender, EventArgs e)
+        {
+            NpgsqlCommand command = new NpgsqlCommand(richTextBox1.Text, Control.connection.SqlConn);
+            //   Control.connection.SqlConn.Open();
+            try
+            {
+                command.ExecuteNonQuery();
+                string str = GetTableName(richTextBox1.Text);
+                string str_show = "Select * from " + str;
+                NpgsqlDataAdapter npgsql = new NpgsqlDataAdapter(str_show, Control.connection.SqlConn);
+                Control.connection.ds = new DataSet();
+                npgsql.Fill(Control.connection.ds);
+                //Control.connection.SqlConn.Close();
+
+                this.dataGridView1.DataSource = null;
+                this.dataGridView1.DataSource = Control.connection.ds;
+                this.dataGridView1.DataMember = Control.connection.ds.Tables[0].TableName;
+            }
+            catch{
+                MessageBox.Show("请检查您的指令！");
+            }
+        }
+    }
+}
